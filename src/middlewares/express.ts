8c9e26db09070b9e26a05da6f70c4d4877f2eddb
Matthew W <matthew.widdi@gmail.com>
2021-01-18 09:06:35 +1100

    Update Express middleware to be explicitly typed as a RequestHandler instead of manually assigning arguments RequestHandler parameter types.

--- src/middlewares/express.ts
+++ src/middlewares/express.ts
@@ -1,21 +1,20 @@
-import {Request, Response, NextFunction} from "express";
+import {RequestHandler} from "express";
 import Session from "../adapters/ExpressAdapter";
 import {SessionOptions} from "../Session";
 
 /**
  * Create and return an Express middleware that attaches the SSE session to the `sse` property of the `res` object.
  */
-const createMiddleware = (options?: SessionOptions) => (
-	req: Request,
-	res: Response,
-	next: NextFunction
+const createMiddleware = (options?: SessionOptions): RequestHandler => (
+	req,
+	res,
+	next
 ): void => {
 	const session = new Session(req, res, options);
 
 	session.onConnect();
 
 	res.sse = session;
-
 	res.push = session.push;
 	res.stream = session.stream;
 
