f292f909d3796620cea677bcff9c0ffb08782f76
Matthew W <matthew.widdi@gmail.com>
2021-05-22 22:52:46 +1000

    Update example projects to append new elements with stream event data to the document body instead of simply logging them to the console.

--- examples/getting-started/public/index.js
+++ examples/getting-started/public/index.js
@@ -1,5 +1,8 @@
 const sse = new EventSource("/sse");
 
-sse.addEventListener("ping", ({data}) => {
-	console.log(data);
+sse.addEventListener("ping", ({type, data: text}) => {
+	const element = document.createElement("pre");
+	element.innerText = `Got '${type}' event: ${text}.`;
+
+	document.body.appendChild(element);
 });
--- examples/streams/public/index.js
+++ examples/streams/public/index.js
@@ -1,5 +1,12 @@
 const sse = new EventSource("/sse");
 
-sse.addEventListener("stream", (event) => {
-	console.log(event);
+sse.addEventListener("stream", ({type, data}) => {
+	const element = document.createElement("pre");
+	element.innerText = `Got event:\n${JSON.stringify({type, data}, null, 2)}`;
+
+	document.body.appendChild(element);
+
+	if (data.done) {
+		sse.close();
+	}
 });
