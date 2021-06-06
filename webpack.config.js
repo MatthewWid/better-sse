3dc1d96689254d4ef9424b3a3d6798fb054c618b
Matthew W <matthew.widdi@gmail.com>
2021-06-06 18:26:06 +1000

    Replace Parcel bundler with webpack.

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require("path");
+
+const config = (env) => ({
+	context: path.resolve(__dirname),
+	entry: "./src/index.ts",
+	output: {
+		filename: "[name].js",
+		path: path.resolve(__dirname, "build"),
+	},
+	module: {
+		rules: [
+			{
+				test: /\.ts$/,
+				loader: "ts-loader",
+			},
+		],
+	},
+	target: "node",
+	resolve: {
+		extensions: [".ts"],
+	},
+	mode: env.production ? "production" : "development",
+	devtool: env.production ? "source-map" : "eval",
+});
+
+module.exports = config;
