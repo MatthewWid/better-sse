9ede1b747689ca54e89965758ffa11f68a91115c
Matthew W <matthew.widdi@gmail.com>
2021-06-12 22:57:18 +1000

    Add unit tests for server disconnection and the correct header field responses.

--- src/Session.test.ts
+++ src/Session.test.ts
@@ -4,7 +4,7 @@ import Session from "./Session";
 
 const host = "127.0.0.1";
 const port = 8080;
-const url = `http://${host}:${port}`;
+const url = `http://${host}:${port}/`;
 
 const createServer = () =>
 	new Promise<http.Server>((resolve, reject) => {
@@ -69,7 +69,7 @@ describe("initialisation", () => {
 		eventsource = new EventSource(url);
 	});
 
-	it("fires the disconnection event when the client kills the connection", async (done) => {
+	it("fires the disconnection event when the client kills the connection", (done) => {
 		server.on("request", (req, res) => {
 			const session = new Session(req, res);
 
@@ -85,5 +85,41 @@ describe("initialisation", () => {
 		eventsource.addEventListener("open", () => {
 			eventsource.close();
 		});
+	});
+
+	it("fires the disconnection event when the server closes the response stream", (done) => {
+		server.on("request", (req, res) => {
+			const session = new Session(req, res);
+
+			session.on("disconnected", () => {
+				done();
+			});
+
+			session.on("connected", () => {
+				res.end();
+			});
+		});
+
+		eventsource = new EventSource(url);
+	});
+
+	it("returns the correct response status code and headers", (done) => {
+		server.on("request", (req, res) => {
+			const session = new Session(req, res);
+
+			session.on("connected", () => {
+				expect(res.headersSent).toBeTruthy();
+
+				expect(res.getHeader("Content-Type")).toBe("text/event-stream");
+				expect(res.getHeader("Cache-Control")).toBe(
+					"no-cache, no-transform"
+				);
+				expect(res.getHeader("Connection")).toBe("keep-alive");
+
+				done();
+			});
+		});
+
+		eventsource = new EventSource(url);
 	});
 });
