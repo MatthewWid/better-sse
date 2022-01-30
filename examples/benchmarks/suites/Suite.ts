9a1f3f3eebb72cb59bbbb766897da29719b8c3c5
Matthew W <matthew.widdi@gmail.com>
2022-01-31 01:19:13 +1100

    Add a custom wrapper for the Benchmark.js Suite class with a cleaner interface that supports promises. Move all existing channel push benchmarks to a separate file in a 'suites' subdirectory.

---
+++ examples/benchmarks/suites/Suite.ts
@@ -0,0 +1,65 @@
+import {Suite as BenchmarkSuite} from "benchmark";
+import express, {Express} from "express";
+
+/**
+ * Wrap the Benchmark.js Suite with a much nicer and more
+ * intuitive interface that also supports promises.
+ */
+export class Suite extends BenchmarkSuite {
+	static port = 8000;
+
+	constructor(name: string, private createBenchmarks: () => Promise<void>) {
+		super(name, {async: true});
+
+		this.on("start", () => {
+			console.log(`Benchmarking "${name}".`);
+		});
+
+		this.on("cycle", (event: Event) => {
+			console.log(String(event.target));
+		});
+
+		this.on("complete", () => {
+			process.exit(0);
+		});
+	}
+
+	/**
+	 * Initialise all benchmarks.
+	 */
+	setup = async () => {
+		await this.createBenchmarks();
+
+		return this;
+	};
+
+	/**
+	 * Set up a new benchmark with a name and setup function.
+	 *
+	 * The setup function takes the web-server, its port, and a function
+	 * that makes it listen on the given port.
+	 *
+	 * The setup function should return an object with a function to *run*
+	 * the code to be benchmarked, and a function to tear down anything
+	 * created in the setup and running process.
+	 */
+	addBenchmark = async (
+		name: string,
+		setup: (
+			server: Express,
+			port: number,
+			listen: () => Promise<void>
+		) => Promise<{run: () => void; teardown: () => void}>
+	): Promise<void> => {
+		const server = express();
+		const port = ++Suite.port;
+
+		const {run, teardown} = await setup(
+			server,
+			port,
+			() => new Promise((resolve) => server.listen(port, () => resolve()))
+		);
+
+		super.add(name, run, {onComplete: teardown});
+	};
+}
---
+++ examples/benchmarks/suites/channel-push-many-sessions.ts
@@ -0,0 +1,80 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+
+import {createSession, createChannel} from "better-sse";
+// @ts-ignore
+import SseChannel from "sse-channel";
+// @ts-ignore
+import EasySse from "easy-server-sent-events";
+import {createClientPool} from "../lib/createClientPool";
+import {Suite} from "./Suite";
+
+export const suite = new Suite("Push events with channels", async () => {
+	const numberOfClients = 10;
+
+	await suite.addBenchmark("better-sse", async (server, port, listen) => {
+		let count = 0;
+
+		const channel = createChannel();
+
+		server.get("/", async (req, res) => {
+			channel.register(await createSession(req, res));
+		});
+
+		await listen();
+
+		return {
+			run: () => {
+				channel.broadcast(++count);
+			},
+			teardown: await createClientPool({port, numberOfClients}),
+		};
+	});
+
+	await suite.addBenchmark("sse-channel", async (server, port, listen) => {
+		let count = 0;
+
+		const channel = new SseChannel({jsonEncode: true});
+
+		server.get("/", (req, res) => {
+			channel.addClient(req, res);
+		});
+
+		await listen();
+
+		return {
+			run: () => {
+				++count;
+
+				channel.send({
+					event: "message",
+					data: count,
+					id: count,
+				});
+			},
+			teardown: await createClientPool({port, numberOfClients}),
+		};
+	});
+
+	await suite.addBenchmark(
+		"easy-server-sent-events",
+		async (server, port, listen) => {
+			let count = 0;
+
+			const {SSE, send} = EasySse({endpoint: "/"});
+
+			server.get("/", (req, res, next) => {
+				SSE(req, res, next);
+				res.flushHeaders();
+			});
+
+			await listen();
+
+			return {
+				run: () => {
+					send("all", "message", ++count);
+				},
+				teardown: await createClientPool({port, numberOfClients}),
+			};
+		}
+	);
+});
