51eb1e30473a5617ff1c2cf2f5ee95945a7bcdb0
Matthew W <matthew.widdi@gmail.com>
2024-10-25 20:52:07 +1100

    docs: rewrite docs from raw markdown to astro+starlight static website

---
+++ docs/astro.config.ts
@@ -0,0 +1,25 @@
+import {defineConfig} from "astro/config";
+import starlight from "@astrojs/starlight";
+
+export default defineConfig({
+	site: "https://matthewwid.github.io",
+	integrations: [
+		starlight({
+			title: "Better SSE",
+			social: {
+				github: "https://github.com/MatthewWid/better-sse",
+			},
+			sidebar: [
+				{
+					label: "Guides",
+					autogenerate: {directory: "guides"},
+				},
+				{
+					label: "Reference",
+					autogenerate: {directory: "reference"},
+				},
+			],
+			customCss: ["./src/styles/custom.css"],
+		}),
+	],
+});
