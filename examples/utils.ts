6855da87311ca39ea0af00f3f69c9bb49288bbd4
Matthew W. <matthew.widdi@gmail.com>
2025-09-15 20:58:12 +1000

    feat(examples/utils): add css frontend file. allow non-existent index.js or index.css

--- examples/utils.ts
+++ examples/utils.ts
@@ -9,15 +9,29 @@ export const getFrontendFiles = (baseDir: string) => {
 
 	const indexHtmlPath = join(publicDir, "./index.html");
 	const indexJsPath = join(publicDir, "./index.js");
+	const indexCssPath = join(publicDir, "./index.css");
 
 	const indexHtmlContent = readFileSync(indexHtmlPath, "utf8");
-	const indexJsContent = readFileSync(indexJsPath, "utf8");
+
+	let indexJsContent = "";
+
+	try {
+		indexJsContent = readFileSync(indexJsPath, "utf8");
+	} catch (_) {}
+
+	let indexCssContent = "";
+
+	try {
+		indexCssContent = readFileSync(indexCssPath, "utf8");
+	} catch (_) {}
 
 	return {
 		publicDir,
 		indexHtmlPath,
 		indexJsPath,
+		indexCssPath,
 		indexHtmlContent,
 		indexJsContent,
+		indexCssContent,
 	};
 };
